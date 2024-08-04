b446629cac60cb22616417a62aa51523de591132
Parthiv <parthivsaikia985@gmail.com>
2024-08-04 19:14:18 +0530

    Initial dist subtree commit

---
+++ bundle.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ui__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ui */ \"./src/ui.js\");\n\n(0,_ui__WEBPACK_IMPORTED_MODULE_0__.loadUI)();\n\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/ui.js":
+/*!*******************!*\
+  !*** ./src/ui.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   loadUI: () => (/* binding */ loadUI)\n/* harmony export */ });\nfunction makeElement(name, className) {\n  const element = document.createElement(name);\n  element.classList.add(className);\n  return element;\n}\n\nfunction addTaskBtn() {\n  const addTaskBtn = makeElement(\"button\", \"add-task-btn\");\n  addTaskBtn.textContent = \"Add Task\";\n  return addTaskBtn;\n}\n\nfunction projectsDiv() {\n  const projectsDiv = makeElement(\"div\", \"projects-div\");\n  const projectsHeadingDiv = makeElement(\"div\", \"projects-heading-div\");\n  const projectsHeading = makeElement(\"h3\", \"projects-heading\");\n  const addProjectBtn = makeElement(\"button\", \"add-project-btn\");\n  addProjectBtn.textContent = \"Add Project\";\n  const showProjectsDiv = makeElement(\"div\", \"show-projects-div\");\n\n  projectsHeadingDiv.append(projectsHeading, addProjectBtn);\n  projectsDiv.append(projectsHeading, showProjectsDiv);\n\n  return projectsDiv;\n}\n\nfunction leftContainer() {\n  const leftContainerDiv = makeElement(\"div\", \"left-container-div\");\n  leftContainerDiv.append(addTaskBtn(), projectsDiv());\n  return leftContainerDiv;\n}\n\nfunction mainContainer() {\n  const mainContainer = makeElement(\"div\", \"main-container\");\n  mainContainer.append(leftContainer());\n  return mainContainer;\n}\n\nfunction loadUI() {\n  const content = document.querySelector(\".content\");\n  content.appendChild(mainContainer());\n}\n\n\n//# sourceURL=webpack://to-do-list/./src/ui.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
